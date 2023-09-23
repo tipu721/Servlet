28ceb35c30199e453069e004775dead8c1592c2e
tipu721 <tsultan721@gmail.com>
2023-09-23 13:03:25 +0600

    Revert "Revert "file upload download""
    This reverts commit 08a8b236e3f351d6261bf651710898d949e135b7.

---
+++ src/main/java/com/tipu/controller/UploadFile.java
@@ -0,0 +1,23 @@
+package com.tipu.controller;
+
+import com.oreilly.servlet.MultipartRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/uploadfile")
+public class UploadFile extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        MultipartRequest multipartRequest = new MultipartRequest(req, "G:/new");
+        printWriter.println("Successfully Uploaded");
+
+    }
+}
